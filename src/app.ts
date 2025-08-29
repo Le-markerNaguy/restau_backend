c435ebf49fb9f58d4cbb6c700bea112411d81627
Le-markerNaguy <naguyjuccedem@email.com>
2025-08-29 21:29:22 +0200

    URL Frontend

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,9 @@ app.use(helmet());
 app.use(express.json({ limit: "2mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: (process.env.FRONTEND_URL?.split(",") as string[]) || "*",
-  credentials: true,
+  origin: (['http://localhost:3000', 'http://localhost:3001','https://restau-frontend.vercel.app']),
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true
 }));
 app.use(morgan("dev"));
 
